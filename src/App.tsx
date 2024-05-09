df79149fd576ab39b60ab2e640e5638518597c4b
pandeyrr <hipandeyrr@gmail.com>
2024-05-09 14:08:17 +0100

    Layout update

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MovieList from "./components/MovieList";
+import AboutPage from "./Pages/AboutPage";
+import Layout from "./Pages/Layout";
+import Contact from "./Pages/Contact";
+import NoPage from "./Pages/NoPage";
 
 export default function App() {
   return (
     <>
-      <div className="container-fluid">
-        <div className="row">
-          <MovieList />
-        </div>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MovieList />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
